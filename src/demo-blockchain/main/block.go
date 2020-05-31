d44dbd3ac95d93355f550be957358b11c7cc7b48
xiejianzhang <xiejianzhangmail@163.com>
2020-05-31 20:27:47 +0800

    20200531 优化区块

--- src/demo-blockchain/main/block.go
+++ src/demo-blockchain/main/block.go
@@ -1,37 +1,70 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha256"
+	"encoding/binary"
+	"log"
+	"time"
 )
 
-/**
-  0.定义结构
-  1.前区块hash
-  2.当前区块hash
-  3.数据
-*/
-
+//1.定义结构
 type Block struct {
+	//1.版本号
+	Version uint64
+	//2.前区块hash
 	PrevHash []byte
-	Hash     []byte
-	Data     []byte
+	//3.Merkle根
+	MerkleRoot []byte
+	//4.时间戳
+	TimeStamp uint64
+	//5.难度值
+	Difficulty uint64
+	//6.随机数，也就是挖矿要找的数据
+	Nonce uint64
+	//a.当前区块hash,正常比特币区块中没有当前的hash
+	Hash []byte
+	//b.数据
+	Data []byte
 }
 
-//4.创建区块
+//实现一个辅助函数,将uint64转成[]byte
+func Uint64ToByte(num uint64) []byte {
+	var buffer bytes.Buffer
+	err := binary.Write(&buffer, binary.BigEndian, num)
+	if err != nil {
+		log.Panic(err)
+	}
+	return buffer.Bytes()
+}
+
+//2.创建区块
 func NewBlock(data string, prevBlockHash []byte) *Block {
 	block := Block{
-		PrevHash: prevBlockHash,
-		Hash:     []byte{}, //先填空，后面在计算
-		Data:     []byte(data),
+		Version:    00,
+		PrevHash:   prevBlockHash,
+		MerkleRoot: []byte{},
+		TimeStamp:  uint64(time.Now().Unix()),
+		Difficulty: 0,
+		Nonce:      0,
+		Hash:       []byte{}, //先填空，后面在计算
+		Data:       []byte(data),
 	}
 	block.SetHash()
 	return &block
 }
 
-//5.生成hash
+//3.生成hash
 func (block *Block) SetHash() {
+	var blockInfo []byte
 	//拼装数据
-	blockInfo := append(block.PrevHash, block.Data...) //将数组打散
+	blockInfo = append(blockInfo, Uint64ToByte(block.Version)...)
+	blockInfo = append(blockInfo, block.PrevHash...)
+	blockInfo = append(blockInfo, block.MerkleRoot...)
+	blockInfo = append(blockInfo, Uint64ToByte(block.TimeStamp)...)
+	blockInfo = append(blockInfo, Uint64ToByte(block.Difficulty)...)
+	blockInfo = append(blockInfo, Uint64ToByte(block.Nonce)...)
+	blockInfo = append(blockInfo, block.Data...)
 	//sha256
 	hash := sha256.Sum256(blockInfo)
 	block.Hash = hash[:]
